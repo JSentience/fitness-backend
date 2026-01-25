8800bd7dde27a2fddec88336ddb400157d08b16d
JSentience <serega2387@gmail.com>
2026-01-25 12:39:47 +0300

    Инициализация
    Добавлены базовые конфигурационные файлы, схема Prisma и миграции, сгенерирован клиент Prisma и исходные модули для аутентификации, пользователей, заказов и рецептов. Включает настройки для компоновки, форматирования.

---
+++ src/recipes/ingredients/ingredients.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common'
+import { IngredientsResolver } from './ingredients.resolver'
+import { IngredientsService } from './ingredients.service'
+
+@Module({
+	providers: [IngredientsResolver, IngredientsService],
+})
+export class IngredientsModule {}
---
+++ src/recipes/recipes.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common'
+import { IngredientsModule } from './ingredients/ingredients.module'
+import { RecipesResolver } from './recipes.resolver'
+import { RecipesService } from './recipes.service'
+
+@Module({
+	providers: [RecipesResolver, RecipesService],
+	imports: [IngredientsModule],
+})
+export class RecipesModule {}
